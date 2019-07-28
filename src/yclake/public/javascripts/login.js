8e780497839cd660d6282edad968208f22b9a2c5
jermaine <jermaine.feng@carel.com>
2019-07-28 21:24:14 +0800

    [add] add pages

--- src/yclake/public/javascripts/login.js
+++ src/yclake/public/javascripts/login.js
@@ -26,42 +26,4 @@ $(document).ready(function () {
             }
         });
     });
-
-
-    // $.ajax({
-    //     type:'POST',
-    //     url:'/',
-    //     data: {
-
-    //     },
-    //     dataType:'json',
-    //     success:function(result){
-    //         if(result.success){
-    //             var data = result.data,
-    //                 length = data.length,
-    //                 sources = {'PTH':[],'SMD':[],'FCT':[],'HUM':[]};
-
-    //             for(var i=0;i<length;i++){
-    //                 sources[data[i].tag].push(data[i].value);
-    //             }
-
-    //             $('#content').find('input.ui-autocomplete-input').each(function(i,e){
-    //                 var _e = $(e),
-    //                     line = _e.parent().prev().prev().text().substr(0,3);
-
-    //                 _e.autocomplete({
-    //                     minLength: 0,
-    //                     scrollHeight:220,
-    //                     source:sources[line]
-    //                 }).focus(function(e) {
-    //                     $(_e).autocomplete('search', '');
-    //                 });
-    //             });
-    //         }
-
-    //     },
-    //     error:function(data){
-    //         report.ui.alert('服务器内部错误，请查看日志<br/>'+data.responseText);
-    //     }
-    // })
 });
