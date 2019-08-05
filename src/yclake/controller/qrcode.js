b9ba03137da00e2366d81720d1f05e5f11bb7d4e
jermaine007 <jermaine007@qq.com>
2019-08-05 21:57:48 +0800

    [MOD] modify qrcode configuration

--- src/yclake/controller/qrcode.js
+++ src/yclake/controller/qrcode.js
@@ -241,7 +241,7 @@ const configData = async (ctx, next) => {
         config.random.identityCodeLength = _codeLength
         const fs = require('fs')
         const path = require('path')
-        fs.writeFileSync(path.join(__dirname, '../config.json'))
+        fs.writeFileSync(path.join(__dirname, '../config.json'), JSON.stringify(config))
         ctx.body = {
           success: true
         }
