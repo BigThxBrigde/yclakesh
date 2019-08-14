ab4df1ee9955633c4181df0590fa14fca9908592
jermaine007 <jermaine007@qq.com>
2019-08-14 22:50:15 +0800

    [Add] add router for video

--- src/yclake/routes/identify.js
+++ src/yclake/routes/identify.js
@@ -3,5 +3,6 @@ const qrcodeController = require('../controller/qrcode')
 router.prefix('/identify')
 
 router.get('/:serialid/:code', qrcodeController.identify)
+router.get('/video', qrcodeController.renderVideo)
 
 module.exports = router
